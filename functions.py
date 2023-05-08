bddf7493fd7ed0f53db938d1990f2b43a91a36e8
istvanloffler <loffleristvan120@gmail.com>
2023-05-08 21:18:58 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+FILEPATH = "todos.txt"
+todos_arg = []
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+    return 0
---
+++ web.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+checkboxes = []
+
+def add_todo():
+    if st.session_state["new_todo"] != "":
+        todo = st.session_state["new_todo"] + '\n'
+        todos.append(todo)
+        functions.write_todos(todos)
+        st.session_state.new_todo = ""
+    else:
+        print("Write todo")
+
+
+def complete_todo():
+    for index, checkbox in enumerate(checkboxes):
+        if checkbox:
+            todos.pop(index)
+            functions.write_todos(todos)
+            del st.session_state[todo]
+
+
+st.title("Todo App")
+st.subheader("This is my todo app")
+st.write("to increase your productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    checkboxes.append(checkbox)
+    #if checkbox:
+        #todos.pop(index)
+        #functions.write_todos(todos)
+        #del st.session_state[todo]
+        #st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+
+st.button(label="Complete", key="Complete", on_click=complete_todo())
+
+st.session_state
